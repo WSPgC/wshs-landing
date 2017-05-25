edd8e11428fb4014a58a3ab0a9f718fa9ee7367d
Zach <zach@kobalt-orange.com>
2017-05-25 14:57:21 -0500

    Started work on redoing sliding navbar. Currently broken, will fix

--- js/scrollNav.js
+++ js/scrollNav.js
@@ -1,42 +1,7 @@
-$(function() {
-  $('#mynav-title').data('size', 'big');
-});
-
-
-$(window).scroll(function() {
-  if ($(document).scrollTop() > 0) {
-    if ($('#mynav-title').data('size') == 'big') {
-      $('#mynav-title').data('size', 'small');
-      $('#mynav-title').transition({
-        height: '100%',
-        'padding-top': 0,
-        'padding-right': 0,
-        'padding-bottom': 0,
-        'padding-left': 0,
-        fontSize: '48px'
-      });
-      $('#mynav-title').removeClass('z-depth-3', 400);
-    }
+$(document).scroll(function() {
+  if ($(this).scrollTop() > 0) { //Adjust 150
+    $('#mynav-title').addClass('shrinked');
   } else {
-    if ($('#mynav-title').data('size') == 'small') {
-      $('#mynav-title').data('size', 'big');
-      $('#mynav-title').transition({
-        height: '175%',
-        'padding-top': 25,
-        'padding-right': 25,
-        'padding-bottom': 25,
-        'padding-left': 25,
-        fontSize: '64px'
-      });
-      // $('#mynav-title').stop().animate({
-      //     height: '175%',
-      //     'padding-top': 25,
-      //     'padding-right': 25,
-      //     'padding-bottom': 25,
-      //     'padding-left': 25,
-      //     fontSize: '64px'
-      // }, 200);
-      $('#mynav-title').addClass('z-depth-3', 400);
-    }
+    $('#mynav-title').removeClass('shrinked');
   }
 });
