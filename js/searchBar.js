bd4e6fce3aa29a3ba7214fd43905e8ff318ecd5f
Zach <zach@kobalt-orange.com>
2017-05-26 15:01:07 -0500

    Worked more on sliding search box, currently broken

--- js/searchBar.js
+++ js/searchBar.js
@@ -1,3 +1,19 @@
-$('#mySearch').click(function() {
-  $(this).toggleClass('collapsed');
+$(document).ready(function() {
+  $('#mySearch').css('width', '0');
+  $('#mySearch').hide();
+});
+
+$('#myTouchSearch').click(function() {
+  $('#search').toggleClass('mySearchOpen');
+  if ($('#search').hasClass('mySearchOpen')) {
+    $('#search').show();
+    $('#search').transition({width: 1000}, 1000)
+
+  } else {
+    $('#search').transition({x: 100}, 500)
+    $('#search').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
+      function(e) {
+        // code to execute after transition ends
+      });
+  }
 });
