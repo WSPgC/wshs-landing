98eb8f1646cdd19c91b8b6bf95c11f4bef87344a
Zach <zach@kobalt-orange.com>
2017-05-28 16:25:26 -0500

    Code beautify

--- js/fileLoader.js
+++ js/fileLoader.js
@@ -1,13 +1,7 @@
-// $(function(){
-//   $('#fileLoader').load('files.html');
-//   $('#nav-placeholder').load('nav.html');
-// });
-
-
-$.get("nav.html", function(data){
-    $("#nav-placeholder").replaceWith(data);
+$.get("nav.html", function(data) {
+  $("#nav-placeholder").replaceWith(data);
 });
 
-$.get("files.html", function(data){
-    $("#fileLoader").replaceWith(data);
+$.get("files.html", function(data) {
+  $("#fileLoader").replaceWith(data);
 });
--- js/init.js
+++ js/init.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('.scrollspy').scrollSpy({
     scrollOffset: 64
   });
