61ffdf714c878576d4f7839eb8266287bdf39699
Zach <zach@kobalt-orange.com>
2017-05-27 21:56:28 -0500

    Basic cleanup, added shorter times for animations for search bar and SOUTH header when they are retracted and made bigger, respectively.

--- js/scrollNav.js
+++ js/scrollNav.js
@@ -1,7 +1,7 @@
-$(document).on("scroll", function() {
-  if ($(document).scrollTop() > 10) {
-    $('#myNav-title').addClass("shrink");
+$(document).on('scroll', function() {
+  if ($(document).scrollTop() > 50) {
+    $('#myNav-title').addClass('shrink');
   } else {
-    $('#myNav-title').removeClass("shrink");
+    $('#myNav-title').removeClass('shrink');
   }
 });
--- js/searchBar.js
+++ js/searchBar.js
@@ -8,24 +8,3 @@ $('#myTouchSearch').click(function() {
   $('#mySearch').addClass('visible');
   $('#search').focus();
 });
-
-
-
-
-
-
-
-
-
-// $('#search').animate({width: 100+'%'}, 500);
-// if ($('#mySearch').hasClass('mySearchOpen')) {
-//   $('#mySearch').show();
-//   $('#mySearch').transition({x: '+=100%'}, 500, 'ease');
-//   $('#search').focus();
-// } else {
-//   $('#mySearch').transition({x: '+= -100%'}, 500, 'ease');
-//   $('#mySearch').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',
-//     function(e) {
-//       $('#mySearch').hide();
-//     });
-// }
