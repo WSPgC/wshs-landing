90b114e213728c3bc6baa5110ba58a1297783f68
Zach <zach@kobalt-orange.com>
2017-05-30 15:02:27 -0500

    More work on chip tagging system

--- js/dirChips.js
+++ js/dirChips.js
@@ -1,5 +1,32 @@
-function sendToChip($dirChip) {
-  var chip = "<div class='chip'>" + $dirChip + "<i class='close material-icons'>close</i></div>";
-  $('#chipLine').append(chip);
+function createChip($dirChip) {
+  var chipName = String($dirChip);
+  var chipID = chipName.replace(/\s+/g, '');
+  var chip = "<div id='"+ chipID +"' class='chip'>" + chipName + "<i class='material-icons add' onclick='addChip(" + chipName + ")'>add</i></div>";
+  $('#addChipLine').append(chip);
+}
 
+createChip('Art');
+createChip('Bilingual');
+createChip('Business and Marketing');
+createChip('English');
+createChip('F.A.C.E');
+createChip('Guidance');
+createChip('Libary');
+createChip('Math');
+createChip('Music');
+createChip('Physical Ed');
+createChip('Science');
+createChip('Secretaries');
+createChip('Social Studies');
+createChip('Special Ed');
+createChip('Support Staff');
+createChip('Tech Ed');
+createChip('World Language');
+
+function addChip($dirChip) {
+  var chipName = String($dirChip);
+  var chipID = chipName.replace(/\s+/g, '');
+  $(chipID).remove();
+  var chip = "<div id='"+ chipID +"' class='chip'>" + chipName + "<i class='material-icons close'>close</i></div>";
+  $('#finalChipLine').append(chip);
 }
